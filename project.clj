cd77f0dd25003480e1441134cfa86305925a2e9c
Robert Krahn <robert.krahn@gmail.com>
2015-02-23 19:27:15 -0800

    addinging services to server

--- project.clj
+++ project.clj
@@ -2,8 +2,12 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.clojure/clojurescript "0.0-2850"]
+                 [org.clojure/clojurescript "0.0-2905"]
+                 [jarohen/chord "0.6.0"]
                  [http-kit "2.1.16"]
-                 [jarohen/chord "0.6.0"]]
+                 [compojure/compojure "1.3.2"]
+                 [javax.servlet/servlet-api "2.5"]
+                 [stylefruits/gniazdo "0.3.1"]
+                 [org.clojure/data.json "0.2.3"]]
   :dev-dependencies [[com.cemerick/clojurescript.test "0.3.3"]]
   :source-paths ["target/classes"])
--- src/clj/rksm/websocket_test/server.clj
+++ src/clj/rksm/websocket_test/server.clj
@@ -1,42 +1,121 @@
 (ns rksm.websocket-test.server
-  (:use org.httpkit.server))
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]
+            [org.httpkit.server :as http]
+            [compojure.core :refer [defroutes GET POST DELETE ANY context]]
+            [clojure.data.json :as json]))
 
-(defonce server (atom nil))
+; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+(declare find-server-by-host-and-port handle-service-request!)
 
 (defn app-simple-http [req]
-  {:status  200
+  {:status 200
   :headers {"Content-Type" "text/html"}
   :body    "hello2 HTTP!"} )
 
 (defn app-streaming-response [req]
-  (with-channel req ch
+  (http/with-channel req ch
     (future
      (Thread/sleep 1000)
-     (send! ch {:status  200
+     (http/send! ch {:status 200
                 :headers {"Content-Type" "text/html"}
                 :body "fooo abbbrrrr"}))))
 
-(defn app [req]
-  (with-channel req channel    
-    (on-close channel (fn [status]
+(defn app-websocket-echo [req]
+  (http/with-channel req channel
+    (http/on-close channel (fn [status]
                         (println "channel closed")))
-    (if (websocket? channel)
+    (if (http/websocket? channel)
       (println "WebSocket channel")
       (println "HTTP channel"))
-    (on-receive channel (fn [data]
-                          (send! channel data)))))
+    (http/on-receive channel (fn [data]
+                          (http/send! channel data)))))
 
+(defn websocket-service-handler [req]
+  (http/with-channel req channel
+    (http/on-close channel (fn [status]))
+    (http/on-receive channel (fn [data]
+                               (let [{host :server-name, port :server-port} req
+                                     server (find-server-by-host-and-port :host host :port port)]
+                                 (handle-service-request! server channel data))))))
 
-(defn stop-server []
-  (when-not (nil? @server)
-    (@server :timeout 100)
-    (reset! server nil)))
+; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-(defn start-server []
-  (reset! server (run-server #'app {:port 8081})))
+(defn app [req]
+   ;; all other, return 404
+
+; (run-server (site #'all-routes) {:port 8080})
+)
+
+(defroutes all-routes
+;   (GET "/" [] show-landing-page)
+  (GET "/ws" [] websocket-service-handler)
+  (route/files "/" {:root "resources" :allow-symlinks? true})
+  (route/not-found "<p>Page not found.</p>"))
+
+; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+(defonce servers (atom []))
+
+(defn find-server-by-host-and-port
+  [& {:keys [host port]}]
+  (first (filter #(and (= host (:host %)) (= port (:port %))) @servers)))
+
+(defn stop-server!
+  [server]
+  (when-not (nil? server)
+    (let [stop (:stop server)]
+      (stop :timeout 100)
+      (swap! servers #(filter (fn [s] (not= s server)) %)))))
+
+(defn stop-all-servers!
+  []
+  (doseq [s @servers] (stop-server! s)))
+
+(defn start-server!
+  [& {:keys [port host], :or {port 8081 host "0.0.0.0"} :as opts}]
+  ;   (reset! server (run-server #'app {:port 8081}))
+  (let [stop (http/run-server #'all-routes {:port port :host host})
+        s {:id (str (java.util.UUID/randomUUID))
+           :stop stop
+           :port port :host host}]
+    (swap! servers conj s)
+    s))
+
+(defn ensure-server!
+  [& opts]
+  (let [{:keys [host port]} (apply hash-map opts)]
+    (or (find-server-by-host-and-port :host host :port port)
+        (apply start-server! opts))))
+
+(defn add-service!
+  [name {id :id :as server} handler]
+  (let [s (first (filter #(= id (:id %)) @servers))]
+    (assert s (str "server with id " id " not found"))
+    (swap! servers (partial replace {s (assoc-in s [:services name] handler)}))))
+
+(defn answer-message-not-understood 
+  [server channel data]
+  (http/send! channel (json/write-str {:error "messageNotUnderstood"})))
+
+(defn handle-service-request!
+  [server channel data]
+  (let [msg (if (string? data) (json/read-str data :key-fn keyword))
+        {:keys [target action]} msg
+        handler (get-in server [:services action])]
+    (cond
+      (not= (:id server) target) (answer-message-not-understood server channel data)
+      (nil? handler) (answer-message-not-understood server channel data)
+      :default (handler channel msg))))
+
+; (defn send!
+;   [& {:keys [target connection data]}]
+;   123)
 
 (comment
  (start-server)
- (stop-server)
- server
+ (stop-server (first @servers))
+ (-> (first @servers) .)
+ servers
  )
