78b16df15aeaba24ea1c471233b72fc7a00a7561
Robert Krahn <robert.krahn@gmail.com>
2015-04-24 23:46:14 -0700

    0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.rksm/cloxp-com "0.1.2"
+(defproject org.rksm/cloxp-com "0.1.3"
   :description "intra- and inter-system messaging for cloxp"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
